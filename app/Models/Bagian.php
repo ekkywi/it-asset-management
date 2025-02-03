6a5cd88906bd336505aa47d614ed935eb425d2a0
Yon Ekky Wijayanto <yonekkywijayanto@outlook.com>
2025-02-03 15:31:10 +0700

    UI Update
    UI Update

---
+++ app/Models/Bagian.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Bagian extends Model
+{
+    use HasFactory;
+
+    protected $table = 'bagian';
+
+    protected $fillable = [
+        'nama_bagian',
+        'tag_bagian',
+        'keterangan'
+    ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+}
